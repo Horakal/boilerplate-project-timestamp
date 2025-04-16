566271ea0eabbc751a6c6f279079fdacea0af9af
Horakal <felipe.reis.e@gmail.com>
2025-04-16 17:43:48 -0300

    resolucao desafio

--- index.js
+++ index.js
@@ -2,31 +2,52 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api/:date?", function (req, res) {
+  var requestDate = req.params.date;
+  if (requestDate === null || requestDate === undefined)
+    return res.json({
+      unix: new Date().getTime(),
+      utc: new Date().toUTCString(),
+    });
 
+  var isUnixTimeStamp = isNumeric(requestDate);
+
+  var isInvalid =
+    new Date(isUnixTimeStamp ? +requestDate : requestDate).getTime() > 0;
+  if (isInvalid === false) return res.json({ error: "Invalid Date" });
+
+  return res.json({
+    unix: isUnixTimeStamp ? +requestDate : new Date(requestDate).getTime(),
+    utc: new Date(isUnixTimeStamp ? +requestDate : requestDate).toUTCString(),
+  });
+});
+
+function isNumeric(str) {
+  return /^\d+$/.test(str);
+}
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
